d0d468b6e8b5284e85dd212c86bbe3010fd91f4b
Ravi Kant Yadav <ravikantyadav1918@gmail.com>
2025-11-14 13:07:19 +0530

    update

--- ett_gns_app/gns_constant.py
+++ ett_gns_app/gns_constant.py
@@ -2,5 +2,14 @@ class AllTemplatesData:
     def __init__(self):
         self.TEMPLATES = {
             "welcome_email.html": ["user_name", "welcome_message"],
-            "password_reset.html": ["user_name", "reset_link"]
-}
+            "password_reset.html": ["user_name", "reset_link"],
+            "universal_template.html": [
+                "heading",
+                "user_name",
+                "body_message",
+                "dynamic_block",
+                "cta_url",
+                "cta_label",
+                "year",
+            ]
+        }
